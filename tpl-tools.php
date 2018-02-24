0bacc372a4b318a5e5b6d75fae18271242ecdc06
David Beauchamp <dbeauchamp@makermedia.com>
2018-02-24 12:16:23 -0500

    Adding the tools page functionality

---
+++ tpl-tools.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Tools Page
+*/
+?>
+<?php get_header(); ?>
+
+<?php get_template_part( '/templates/page/page-header' ); ?>
+
+<div class="page-content padding-small" id="home-container">
+  <div class="container">
+    <div class="row">
+      <div class="md-column col-md-8">
+        <?php get_template_part( '/templates/blog/posts-loop-tools' ); ?>
+      </div>
+
+      <div class="md-column col-side col-md-4 col-side">
+        <?php dynamic_sidebar('sidebar-blog'); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
